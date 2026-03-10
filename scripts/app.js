63761ee754a50acb44e17cfe99c47ac05f7922d0
Pratham Yadav <prathamyadav219@gmail.com>
2026-03-10 19:56:44 +0530

    Added navbar and product cards UI

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+console.log("Ecommerce site running");
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(btn=>{
+btn.addEventListener("click",()=>{
+alert("Product added to cart");
+});
+});
