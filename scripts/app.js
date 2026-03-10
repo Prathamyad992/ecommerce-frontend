1b0fcceb38191405e68eb5d60ca9446cfb1a37db
Pratham Yadav <prathamyadav219@gmail.com>
2026-03-10 20:01:35 +0530

    Added layout, navbar and product section

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,9 @@
-console.log("Ecommerce site running");
+console.log("E-Commerce site loaded");
 
 const buttons = document.querySelectorAll("button");
 
-buttons.forEach(btn=>{
-btn.addEventListener("click",()=>{
-alert("Product added to cart");
+buttons.forEach(button => {
+button.addEventListener("click", () => {
+alert("Item added to cart");
 });
 });
